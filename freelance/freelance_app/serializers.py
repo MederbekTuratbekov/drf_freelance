edfdc1ef0d6622f29fc27a2b2d74622cd29a72ba
Neo <mrrneo@outlook.com>
2026-06-07 07:21:57 +0600

    ...

--- freelance/freelance_app/serializers.py
+++ freelance/freelance_app/serializers.py
@@ -7,8 +7,7 @@ from django.contrib.auth import authenticate
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
-        fields = ('username', 'email', 'password', 'first_name', 'last_name',
-                  'age', 'phone_number', 'status', 'date_registered')
+        fields = ('username', 'email', 'password', 'first_name', 'last_name', 'role', 'bio', 'avatar', 'skills')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
@@ -25,6 +24,7 @@ class UserSerializer(serializers.ModelSerializer):
             'access': str(refresh.access_token),
             'refresh': str(refresh),
         }
+
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
@@ -46,51 +46,61 @@ class LoginSerializer(serializers.Serializer):
             'access': str(refresh.access_token),
             'refresh': str(refresh),
         }
-# ---------------------------------------------------------------------------------
+
+
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
-        fields = ('username', 'email', 'password', 'first_name', 'last_name',
-                  'role', 'bio', 'avatar', 'skills')
+        fields = ('username', 'email', 'password', 'first_name', 'last_name', 'role', 'bio', 'avatar', 'skills')
         extra_kwargs = {'password': {'write_only': True}}
+
 
 class SkillSerializer(serializers.ModelSerializer):
     class Meta:
         model = Skill
-        fields = ('id', 'skill_name',)
+        fields = ('id', 'skill_name')
+
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ('id', 'category_name',)
+        fields = ('id', 'category_name')
+
 
 class ProjectSerializer(serializers.ModelSerializer):
     category = CategorySerializer(read_only=True)
-    skills_required = SkillSerializer(many=True, read_only=True, allow_null=True)
+    skills_required = SkillSerializer(many=True, read_only=True)
     client = UserProfileSerializer(read_only=True)
+
     class Meta:
         model = Project
         fields = ('id', 'title', 'description', 'budget', 'deadline', 'status')
 
+
 class ProjectDetailSerializer(serializers.ModelSerializer):
     category = CategorySerializer(read_only=True)
-    skills_required = SkillSerializer(many=True, read_only=True, allow_null=True)
+    skills_required = SkillSerializer(many=True, read_only=True)
     client = UserProfileSerializer(read_only=True)
+
     class Meta:
         model = Project
         fields = ('id', 'title', 'description', 'budget', 'deadline', 'status', 'category', 'skills_required', 'client')
 
+
 class OfferSerializer(serializers.ModelSerializer):
     project = ProjectSerializer(read_only=True)
     freelancer = UserProfileSerializer(read_only=True)
+
     class Meta:
         model = Offer
         fields = ('id', 'project', 'freelancer', 'message', 'proposed_budget', 'proposed_deadline')
 
+
 class ReviewSerializer(serializers.ModelSerializer):
     project = ProjectSerializer(read_only=True)
-    owner_reviewer = UserProfileSerializer(read_only=True)
+    owner_review = UserProfileSerializer(read_only=True)
     target = UserProfileSerializer(read_only=True)
+
     class Meta:
         model = Review
-        fields = ('id', 'project', 'comment', 'rating_review', 'owner_reviewer', 'target')
+        fields = ('id', 'project', 'comment', 'rating_review', 'owner_review', 'target')
--- freelance/freelance_app/views.py
+++ freelance/freelance_app/views.py
@@ -1,17 +1,15 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, generics, status
 from .permissions import CheckIsClient, CheckIsFreelancer
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import generics
 from rest_framework.filters import OrderingFilter, SearchFilter
 from .models import UserProfile, Skill, Category, Project, Offer, Review
-from .serializers import (UserProfileSerializer,
-                          SkillSerializer, CategorySerializer,
+from .serializers import (UserProfileSerializer, SkillSerializer, CategorySerializer,
                           ProjectSerializer, ProjectDetailSerializer,
-                          OfferSerializer, ReviewSerializer, UserSerializer)
+                          OfferSerializer, ReviewSerializer,
+                          UserSerializer, LoginSerializer)
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework import status
 
 
 class RegisterView(generics.CreateAPIView):
@@ -23,6 +21,7 @@ class RegisterView(generics.CreateAPIView):
         user = serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+
 class CustomLoginView(TokenObtainPairView):
     serializer_class = LoginSerializer
 
@@ -32,9 +31,8 @@ class CustomLoginView(TokenObtainPairView):
             serializer.is_valid(raise_exception=True)
         except Exception:
             return Response({"detail": "Неверные учетные данные"}, status=status.HTTP_401_UNAUTHORIZED)
-
-        user = serializer.validated_data
         return Response(serializer.data, status=status.HTTP_200_OK)
+
 
 class LogoutView(generics.GenericAPIView):
     def post(self, request, *args, **kwargs):
@@ -45,82 +43,87 @@ class LogoutView(generics.GenericAPIView):
             return Response(status=status.HTTP_205_RESET_CONTENT)
         except Exception:
             return Response(status=status.HTTP_400_BAD_REQUEST)
-# ---------------------------------------------------------------------------------
+
+
 class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
-
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         return UserProfile.objects.filter(id=self.request.user.id).order_by('id')
 
+
 class OtherUserProfileRetrieveAPIView(generics.RetrieveAPIView):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
-permission_classes = [permissions.IsAuthenticated]
 
 class ProjectAPIView(generics.ListCreateAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
-
     filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
-    ordering_fields = ['category__category_name', '-budget', 'deadline']
+    ordering_fields = ['category__category_name', 'budget', 'deadline']
     filterset_fields = ['category__category_name', 'status']
     search_fields = ['title', 'description']
-
     permission_classes = [permissions.IsAuthenticated]
+
 
 class ProjectDetailAPIView(generics.RetrieveAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectDetailSerializer
-
     permission_classes = [permissions.IsAuthenticated, CheckIsClient]
+
 
 class UserProjectsListAPIView(generics.ListAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
-
     permission_classes = [permissions.IsAuthenticated, CheckIsClient]
+
+    def get_queryset(self):
+        return Project.objects.filter(client=self.request.user)
+
 
 class OfferListAPIView(generics.ListAPIView):
     queryset = Offer.objects.all()
     serializer_class = OfferSerializer
-
     permission_classes = [permissions.IsAuthenticated, CheckIsFreelancer]
+
 
 class UserOfferListAPIView(generics.ListAPIView):
     queryset = Offer.objects.all()
     serializer_class = OfferSerializer
-
     permission_classes = [permissions.IsAuthenticated, CheckIsFreelancer]
 
     def get_queryset(self):
-        return UserProfile.objects.filter(id=self.request.user.id)
+        return Offer.objects.filter(freelancer=self.request.user)
+
 
 class OfferUpdateAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Offer.objects.all()
     serializer_class = OfferSerializer
-
     permission_classes = [permissions.IsAuthenticated, CheckIsFreelancer]
 
     def get_queryset(self):
-        return UserProfile.objects.filter(id=self.request.user.id)
+        return Offer.objects.filter(freelancer=self.request.user)
+
 
 class ReviewCreateAPIView(generics.CreateAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
-
     permission_classes = [permissions.IsAuthenticated]
+
 
 class CategoryListAPIView(generics.ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
+
 class CategoryRetrieveAPIView(generics.RetrieveAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
 
 class SkillListAPIView(generics.ListAPIView):
     queryset = Skill.objects.all()

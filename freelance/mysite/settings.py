027448460a5fd2ce19c32dfd547f8f3b21e9b623
MederbekTuratbekov <142796921+MederbekTuratbekov@users.noreply.github.com>
2026-08-04 20:26:23 +0600

    ...

--- freelance/mysite/settings.py
+++ freelance/mysite/settings.py
@@ -68,8 +68,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'postgres'),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
+        'HOST': os.getenv('POSTGRES_HOST', 'db'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
